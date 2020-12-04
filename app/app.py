893d5cec8e8c0c6283760a7780e4e87c85229bf8
harikrushna <hp52@njit.edu>
2020-12-03 22:51:16 -0500

    Task: Email confirmation and password reset modules are added

--- app/app.py
+++ app/app.py
@@ -1,14 +1,11 @@
-from typing import List, Dict
-import simplejson as json
-from flask import Flask, render_template, flash, redirect, url_for, session, request, logging
+import datetime
+
+from flask import Flask, render_template, flash, redirect, url_for, session, request
 from flaskext.mysql import MySQL
 from pymysql.cursors import DictCursor
-from wtforms import Form, StringField, TextAreaField, PasswordField, validators
-from passlib.hash import sha256_crypt
-from functools import wraps
-from itsdangerous import URLSafeTimedSerializer, SignatureExpired
+from itsdangerous import URLSafeTimedSerializer
+from flask_mail import Mail, Message
 import os
-# from token import confirm_token, generate_confirmation_token
 
 app = Flask(__name__)
 mysql = MySQL(cursorclass=DictCursor)
@@ -20,6 +17,16 @@ app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
 app.config['MYSQL_DATABASE_PORT'] = 3306
 app.config['MYSQL_DATABASE_DB'] = 'covidInsight'
 
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_USERNAME'] = 'covid19.insightlook@gmail.com'
+app.config['MAIL_PASSWORD'] = 'Covid@1999'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USE_SSL'] = True
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_DEFAULT_SENDER'] = 'hkstone14@gmail.com'
+app.config['SECRET_KEY'] = 'my_precious'
+app.config['SECURITY_PASSWORD_SALT'] = 'my_precious_two'
+mail = Mail(app)
 mysql.init_app(app)
 
 s = URLSafeTimedSerializer('Thisisasecret!')
@@ -50,30 +57,67 @@ def add_user():
         flash('Account already exist with given email address!', 'error')
         return render_template('register.html')
     elif user_username:
-        flash('Username ' + username + ' is already taken', 'error')
+        flash('Username ' + username + ' is already taken!', 'error')
+        return render_template('register.html')
     else:
+        email_confirm = 'Thanks for signing up for covid-19 insight. Please follow this link to activate your account:'
+        token = generate_confirmation_token(email)
+        confirm_url = url_for('confirm_email', token=token, _external=True)
+        html = render_template('email.html', confirm_url=confirm_url, email_confirm=email_confirm)
+        subject = "Please confirm your email"
+        send_email(email, subject, html)
+        flash('A confirmation email has been sent via email.', 'success')
         cursor.execute('INSERT INTO users (email, username, password) VALUES(%s, %s, %s)', (email, username, password))
         mysql.get_db().commit()
-        flash('Your account registered successfully.', 'success')
-        return render_template('login.html')
+        return redirect(url_for("index"))
 
 
-# @app.route('/confirm/<token>')
-# def confirm_email(token):
-#     try:
-#         email = confirm_token(token)
-#     except:
-#         flash('The confirmation link is invalid or has expired.', 'danger')
-#
-#     if user.confirmed:
-#         flash('Account already confirmed. Please login.', 'success')
-#     else:
-#         user.confirmed = True
-#         user.confirmed_on = datetime.datetime.now()
-#         db.session.add(user)
-#         db.session.commit()
-#         flash('You have confirmed your account. Thanks!', 'success')
-#     return redirect(url_for('login'))
+@app.route('/confirm/<token>')
+def confirm_email(token):
+    try:
+        email = confirm_token(token)
+    except:
+        flash('The confirmation link is invalid or has expired.', 'danger')
+    cursor = mysql.get_db().cursor()
+    cursor.execute('SELECT * FROM users WHERE email= %s', email)
+    user = cursor.fetchone()
+    # user = User.query.filter_by(email=email).first_or_404()
+    if user:
+        flash('You have confirmed your account. Thanks!', 'success')
+    return redirect(url_for('index'))
+
+
+@app.route('/reset/<token>', methods=['GET', 'POST'])
+def reset_with_token(token):
+    return render_template("resetPassword.html", token=token)
+
+
+def generate_confirmation_token(email):
+    serializer = URLSafeTimedSerializer(app.config['SECRET_KEY'])
+    return serializer.dumps(email, salt=app.config['SECURITY_PASSWORD_SALT'])
+
+
+def confirm_token(token, expiration=3600):
+    serializer = URLSafeTimedSerializer(app.config['SECRET_KEY'])
+    try:
+        email = serializer.loads(
+            token,
+            salt=app.config['SECURITY_PASSWORD_SALT'],
+            max_age=expiration
+        )
+    except:
+        return False
+    return email
+
+
+def send_email(to, subject, template):
+    msg = Message(
+        subject,
+        recipients=[to],
+        html=template,
+        sender=app.config['MAIL_DEFAULT_SENDER']
+    )
+    mail.send(msg)
 
 
 @app.route('/home')
@@ -102,6 +146,52 @@ def login():
         else:
             flash('Incorrect username/password', 'error')
             return render_template('login.html')
+
+
+@app.route('/forgot')
+def forgot():
+    return render_template("forgotPassword.html")
+
+
+@app.route('/reset_password', methods=['PUT', 'POST'])
+def reset_password():
+    cursor = mysql.get_db().cursor()
+    email = request.form['email']
+    cursor.execute('SELECT * FROM users WHERE email= %s', email)
+    user_email = cursor.fetchone()
+    if user_email:
+        confirmed = 'false'
+        reset_pass = 'Please click on the below link to reset your password:'
+        token = generate_confirmation_token(email)
+        confirm_url = url_for('reset_with_token', token=token, _external=True)
+        html = render_template('email.html', confirm_url=confirm_url, reset_pass=reset_pass, confirmed=confirmed)
+        subject = "Password reset requested"
+        send_email(email, subject, html)
+        flash('The password reset link has been sent via email.', 'success')
+        return render_template("forgotPassword.html")
+    else:
+        flash('The email you entered does not exist!', 'error')
+        return render_template("forgotPassword.html")
+
+
+@app.route('/change_password/<token>', methods=['POST'])
+def change_password(token):
+    try:
+        email = confirm_token(token)
+    except:
+        flash('The password reset link is invalid or has expired.', 'danger')
+    cursor = mysql.get_db().cursor()
+    password = request.form['password']
+    cursor.execute('SELECT * FROM users WHERE email= %s', email)
+    user = cursor.fetchone()
+    if user['password'] == password:
+        flash('Your new password can not be the same as the last one!', 'error')
+        return render_template("resetPassword.html", token=token)
+    else:
+        cursor.execute('UPDATE users SET password = %s WHERE email= %s', (password, email))
+        mysql.get_db().commit()
+        flash('Your password has been updated.', 'success')
+        return redirect(url_for('index'))
 
 
 # Logout
